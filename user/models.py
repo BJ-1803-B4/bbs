6cc57968a52d7eca363025331c414827ba60d1bb
youran0 <865809134@qq.com>
2018-07-28 21:15:51 +0800

    新增用户登录注册模板

--- user/models.py
+++ user/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password, check_password
 from django.db import models
 
 
@@ -5,12 +6,15 @@ class UserManager(models.Manager):
     def all(self):
         return super().all().filter(is_delete=False)
 
-    def create_user(self, username, password, email):
+    def create_user(self, username, password):
         user = self.model()
         user.username = username
-        user.email = email
-        user.password = password
+        user.password = make_password(password)
         return user
+
+    # 通过加密算法验证密码
+    def valid_password(self, password):
+        return check_password(password, self.model().password)
 
     # 在这里添加模型管理方法
 
@@ -18,7 +22,11 @@ class UserManager(models.Manager):
 class User(models.Model):
     username = models.CharField(max_length=16, unique=True)
     password = models.CharField(max_length=256)
-    email = models.EmailField()
+    img = models.CharField(max_length=256)
+    post_count = models.IntegerField(default=0)
+    comm_count = models.IntegerField(default=0)
+    follower_count = models.IntegerField(default=0)
+    following_count = models.IntegerField(default=0)
     is_delete = models.BooleanField(default=False)
 
     objects = UserManager()
--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from user import views
 
 urlpatterns = [
-
+    url('^register/$', views.register, name='register'),
+    url('^login/$', views.login, name='login'),
 ]
