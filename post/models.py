8bcdb933d347cca35876bffed78eda9b2a2ba79b
youran0 <865809134@qq.com>
2018-07-28 13:02:39 +0800

    新增了Post模型

--- post/models.py
+++ post/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class PostManager(models.Manager):
+    def all(self):
+        return super().all().filter(is_delete=False)
+
+    def create_post(self, title, content_html, content_str):
+        post = self.model()
+        post.title = title
+        post.content_html = content_html
+        post.content_str = content_str
+        return post
+
+    # 在这里添加模型管理方法
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content_html = models.TextField()
+    content_str = models.TextField()
+    is_delete = models.BooleanField(default=False)
+
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    objects = PostManager()
--- post/views.py
+++ post/views.py
@@ -1,16 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-
-
-    
-
     return render(request, 'nav_common.html')
-
 
 def post_detail(request):
     data = {"username": "lili"}
     return render(request, "post_detail.html", context=data)
-    # return HttpResponse("帖子细节页面")
+
+def search_results(request):
+    return
