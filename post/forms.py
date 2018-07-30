a436bfe7e4b72f532264668328684baec0c68424
youran0 <865809134@qq.com>
2018-07-30 20:40:30 +0800

    1. 修改了帖子详情页面html和css 2. 修改了帖子创建页面html和css

--- post/forms.py
+++ post/forms.py
@@ -4,7 +4,8 @@ from django import forms
 class PostEditForm(forms.Form):
     post_title = forms.CharField(max_length=50,
                                  widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '标题'}))
-    post_content = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control',  'placeholder': '正文'}))
+    post_content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control',  'placeholder': '正文'}))
+
 
     def clean(self):
         cleaned_data = self.cleaned_data
--- post/views.py
+++ post/views.py
@@ -27,10 +27,11 @@ def login_required(view_fun):
     return valid_cookie_and_session
 
 
-def get_username(uid):
+# 将经过加密的uid解密为username
+def get_username(request):
+    uid = request.COOKIES.get('uid', 0)
     if not uid:
         return uid
-    print('onasiondoiansoidn')
     timestamp_signing = signing.TimestampSigner()
     result = timestamp_signing.unsign(uid, max_age=60 * 60 * 24)
     username = signing.loads(result)['username']
@@ -38,8 +39,7 @@ def get_username(uid):
 
 
 def index(request):
-    uid = request.COOKIES.get('uid', 0)
-    username = get_username(uid)
+    username = get_username(request)
     posts = Post.objects.all().order_by('-timestamp')[:20]
     return render(request, 'index.html', {'username': username, 'posts': posts})
 
@@ -48,35 +48,37 @@ def index(request):
 @login_required
 def post_edit(request):
     form = PostEditForm()
+    username = get_username(request)
     if request.method == 'POST':
         form = PostEditForm(request.POST)
         if form.is_valid():
-            username = get_username(request.COOKIES.get('uid'))
             user = User.objects.filter(username=username)[0]
             title = form.cleaned_data['post_title']
             cont_str = form.cleaned_data['post_content']
             cont_html = cont_str
             post = Post.objects.create(user=user, author=username, title=title, cont_str=cont_str, cont_html=cont_html)
             return redirect(reverse('post_detail', args=(post.id,)))
-    return render(request, 'post_edit.html', {'form': form})
+    return render(request, 'post_edit.html', {'form': form, 'username': username})
 
 
 def post_detail(request, pid):
     post = get_object_or_404(Post, pk=pid)
-    return render(request, "post_detail.html", {'post': post})
+    username = get_username(request)
+    return render(request, "post_detail.html", {'post': post, 'username': username})
 
 
 def search_results(request):
-    return render(request, 'search_results.html')
+    username = get_username(request)
+    return render(request, 'search_results.html', {'username': username})
 
 
+# js异步获取帖子列表
 def get_posts(request):
     if request.method == 'GET':
         offset = int(request.GET.get('offset'))
         start = int(request.GET.get('start'))
         queryset = Post.objects.order_by('-timestamp').filter(pk__in=range(start, start + offset))
-        posts = serialize('json', queryset)
-        print(posts)
+        posts = serialize('json', queryset)     # 将查询集序列化成json
         response = JsonResponse({'data': posts})
         return response
     raise Http404
